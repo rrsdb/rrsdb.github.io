1a4c3e5467d8f1ec4d6834cdc3ca5146387edeec
rrsdb <160359636+rrsdb@users.noreply.github.com>
2024-05-17 16:18:43 -0400

    Added more support

--- rrsdb_math.js
+++ rrsdb_math.js
@@ -1,4 +1,5 @@
 
+// Returns the expansion of poly1 * poly2.
 function rrsdb_product(terms, poly1, poly2)
 {
   var coefficients = new Array(terms).fill(0);
@@ -12,6 +13,7 @@ function rrsdb_product(terms, poly1, poly2)
   return coefficients;
 }
 
+// Returns the expansion of 1/(1-q^magnification).
 function rrsdb_expand_geometric(terms, magnification)
 {
   var coefficients = new Array(terms).fill(0);
@@ -25,6 +27,7 @@ function rrsdb_expand_geometric(terms, magnification)
   return coefficients;
 }
 
+// Returns the expansion of 1/(1-q^magnification)^power.
 function rrsdb_expand_geometric_power(terms, power, magnification)
 {
   var geometric_series = rrsdb_expand_geometric(terms, magnification);
@@ -38,6 +41,30 @@ function rrsdb_expand_geometric_power(terms, power, magnification)
   return coefficients;
 }
 
+// Returns the expansion of (1-q^magnification)^power.
+function rrsdb_expand_binomial_power(terms, power, magnification)
+{
+  var factor = new Array(terms).fill(0);
+
+  factor[0] = 1;
+
+  if (magnification >= terms) {
+    return factor;
+  } else {
+    factor[magnification] = -1;
+    
+    var coefficients = factor;
+
+    while (power != 1) {
+      coefficients = rrsdb_product(terms, coefficients, factor);
+      power -= 1;
+    }
+
+    return coefficients;
+  }
+}
+
+// Returns the expansion of (q^offset; q^modulus)^power.
 function rrsdb_expand_q_pochhammer(terms, modulus, power, offset)
 {
   var coefficients = new Array(terms).fill(0);
@@ -45,22 +72,30 @@ function rrsdb_expand_q_pochhammer(terms, modulus, power, offset)
   coefficients[0] = 1;
 
   if (power == 0) return coefficients;
-  
+
   if (power < 0) {
     var index = offset;
-    
+
     while (index < terms) {
       coefficients = rrsdb_product(terms, coefficients,
         rrsdb_expand_geometric_power(terms, -power, index));
       index += modulus;
     }
   } else {
-      
+    var index = offset;
+    
+    while (index < terms) {
+      coefficients = rrsdb_product(terms, coefficients,
+        rrsdb_expand_binomial_power(terms, power, index));
+      index += modulus;
+    }
   }
-  
+
   return coefficients;
 }
 
+// Returns the expansion of
+// (q; q^modulus)^(-power_list[0]) * ... * (q^(modulus - 1); q^modulus)^(-power_list[modulus - 1]).
 function rrsdb_expand_q_pochhammer_product(terms, modulus, power_list)
 {
   var coefficients = new Array(terms).fill(0);
@@ -69,12 +104,14 @@ function rrsdb_expand_q_pochhammer_product(terms, modulus, power_list)
 
   for (var index = 0; index < modulus; ++index) {
     coefficients = rrsdb_product(terms, coefficients,
-      rrsdb_expand_q_pochhammer(terms, modulus, power_list[index], index + 1));
+      rrsdb_expand_q_pochhammer(terms, modulus, -power_list[index], index + 1));
   }
 
   return coefficients;
 }
 
+// Computes the series expansion of a single product signature and writes
+// it to the page in a human readable format.
 function rrsdb_series_expand_simple(modulus, power_list)
 {
   var terms_text_input = document.getElementById("series_expansion_input_box").value.trim();
@@ -99,10 +136,12 @@ function rrsdb_series_expand_simple(modulus, power_list)
   var html = "1";
 
   if (terms > 1) {
-    html += " + " +  (coefficients[1] == 1 ? "" : coefficients[1].toString()) + "q";
-
-    for (var index = 2; index < terms; ++index) {
-      html += " + " + (coefficients[index] == 1 ? "" : coefficients[index].toString()) + "q<sup>" + index.toString() + "</sup>";
+    for (var index = 1; index < terms; ++index) {
+      if (coefficients[index] != 0) {
+        html += (coefficients[index] > 0 ? " + " : " - ")
+          + (Math.abs(coefficients[index]) == 1 ? "" : Math.abs(coefficients[index]).toString())
+          + "q<sup>" + (index == 1 ? "" : index.toString()) + "</sup>";
+      }
     }
   }
 
