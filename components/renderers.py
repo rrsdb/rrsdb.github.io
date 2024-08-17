26fd7532ff162a7ba79a32ad08089721955d3db1
KG <kgscience@hotmail.com>
2024-08-17 15:01:13 -0400

    The whole build

---
+++ components/renderers.py
@@ -0,0 +1,85 @@
+import mistune
+import re
+
+from collections import namedtuple
+
+
+PLUGINS = [
+    "strikethrough",
+    "footnotes",
+    "table",
+    "def_list",
+    "abbr",
+    "mark"
+]
+
+
+def closing_tags(text: str) -> str:
+    return "\n".join(f"</{tag}>" for tag in re.findall(r"<(\w+)(?!.*</\1>)", text))
+
+def wrap(text: str, tags: str) -> str:
+    return f"{tags}\n{text}\n{closing_tags(tags)}"
+
+
+class TextRenderer(mistune.HTMLRenderer):
+    # Really hacky tbh
+    def __getattribute__(self, item):
+        try:
+            if item in object.__getattribute__(self, "__methods") and getattr(type(self), item) == getattr(mistune.HTMLRenderer, item):
+                return lambda text, *args, **kwargs: text
+            
+        except AttributeError:
+            pass
+        
+        return object.__getattribute__(self, item)
+    
+
+plaintext = mistune.create_markdown(renderer=TextRenderer(), plugins=PLUGINS)
+    
+Heading = namedtuple("Heading", ["level", "id", "plaintext", "rendered"])
+
+class RRSDBRenderer(mistune.HTMLRenderer):
+    heading_tags = {}
+
+    inclusive_block_tags = {
+        "h1": '<div id="main">',
+        "h2": '<div class="main_infobox">'
+    }
+    
+    exclusive_block_tags = {}
+    
+    def __init__(self):
+        super().__init__()
+
+        self.headings = []
+        self._heading_stack = [Heading(0, "", "", "")]
+        
+    def heading(self, text: str, level: int, **attrs) -> str:
+        text, heading_id = re.search(r"(.+)\s*(?:\{#(.+)})?", text).groups()
+
+        rendered = ""
+        while level <= self._heading_stack[-1].level:
+            previous = self._heading_stack.pop()
+            rendered += closing_tags(previous.rendered)
+        
+        key = f"h{level}"
+        rendered += self.inclusive_block_tags.get(key, "")
+        rendered += wrap(super().heading(text, level, id=heading_id, **attrs), self.heading_tags.get(key, ""))
+        rendered += self.exclusive_block_tags.get(key, "")
+        
+        heading = Heading(level, heading_id, plaintext(text), rendered)
+        self.headings.append(heading)
+        self._heading_stack.append(heading)
+        
+        return rendered
+    
+    def strong(self, text: str) -> str:
+        return f"<b>{text}</b>"
+
+
+class IdentityRenderer(RRSDBRenderer):
+    inclusive_block_tags = {
+        "h1": '<div id="main">'
+              '<div class="main_infobox">',
+        "h2": '<div class="identity_container">'
+    }
---
+++ pages/identities/template.md
@@ -0,0 +1,51 @@
+# Article for infinite product with [signature](../product_signature.html) SIGNATURE HERE
+
+!!graphs
+
+$$ F(q) := \sum_{n=0}^\infty r(n)q^n := PRODUCT\;REPRESENTATIONS\;HERE $$
+
+## Combinatorial interpretation of product
+
+- $r(n)$ is the number of [partitions](../partitions.html#integer_partitions) of $n$ with CONGRUENCES HERE.
+
+## [Asymptotic formula](../asymptotics.html)
+
+$ \displaystyle r(n) \sim FORMULA\;HERE $ as $ n\to\infty. $
+
+## Power series expansion
+
+!!power_series
+
+## $q$-series representations
+
+$ \displaystyle F(q) = Q\;SERIES\;HERE $
+
+**Common name:** NAME HERE, OR DELETE THIS LINE
+
+**[Bailey pair](../Bailey_pairs.html) proof:** PROOF HERE OR DELETE THIS LINE
+
+**Combinatorial interpretations:**
+- 1st interpretation here
+- 2nd interpretation here
+    
+**Notes:**
+- 1st fact here
+- 2nd fact here
+
+$ \displaystyle F(q) = OTHER\;FORM\;HERE\;OR\;DELETE\;DIV $
+
+**Common name:** NAME HERE, OR DELETE THIS LINE
+
+**[Bailey pair](../Bailey_pairs.html) proof:** PROOF HERE OR DELETE THIS LINE
+
+**Combinatorial interpretations:**
+- 1st interpretation here
+- 2nd interpretation here
+    
+**Notes:**
+- 1st fact here
+- 2nd fact here
+    
+## References
+- Ref 1 here
+- Ref 2 here
