421ede9cb52328c636480fdab0414e942e8afa60
KG <kgscience@hotmail.com>
2024-12-14 19:21:53 -0500

    Use more flexible replacement syntax

--- components/pages.py
+++ components/pages.py
@@ -57,13 +57,14 @@ class RRSDBPage(metaclass=Built):
         self.references = regex.findall(rf"({auth}|(?:{auth}, )*{auth},? (?:and|&) {auth}|) {details}", page)
         
     def __build__(self):
-        self.sidebar = local_path("sidebar.html").read_text(encoding="utf-8")
-        self.header = local_path("header.html").read_text(encoding="utf-8")
-        self.footer = local_path("footer.html").read_text(encoding="utf-8")
-        
+        self.sidebar = self.replace_vars(local_path("sidebar.html").read_text(encoding="utf-8"))
+        self.header = self.replace_vars(local_path("header.html").read_text(encoding="utf-8"))
+        self.footer = self.replace_vars(local_path("footer.html").read_text(encoding="utf-8"))
+
+        self.content = self.replace_vars(local_path(self._parent).with_suffix(".html").read_text(encoding="utf-8"))
+
         # Widgets
-        self.content = local_path(self._parent).with_suffix(".html").read_text(encoding="utf-8").format(**vars(self))
-        self.content = regex.sub(r"!!(\w+)", lambda match: build_widget(match[1]).format(**vars(self)), self.content)
+        self.content = regex.sub(r"!!(\w+)(!!)?", lambda match: self.replace_vars(build_widget(match[1])), self.content)
         
     def __class_getitem__(cls, item):
         return cls._page_types[item]
@@ -71,11 +72,21 @@ class RRSDBPage(metaclass=Built):
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls._page_types[cls._parent] = cls
+
+    # Replace vars in a file (!! shouldn't conflict with other syntax)
+    def replace_vars(self, content: str) -> str:
+        return regex.sub(r"!!(\w+)(!!)?", lambda match: vars(self)[match[1]], content)
         
         
 class InfoPage(RRSDBPage):
     _parent = "pages"
     _renderer = RRSDBRenderer
+
+    def __init__(self, filename: str, page: str):
+        super().__init__(filename, page)
+
+        # Maybe don't hardcode this in the future?
+        self.section = "Tools" if filename == "series_factorization_tool.html" else "Definitions and Preliminaries"
 
 
 class IdentityPage(RRSDBPage):
@@ -91,3 +102,5 @@ class IdentityPage(RRSDBPage):
 
         else:
             self.signature = "{length}, [{items}]".format(**signature.groupdict())
+
+        self.section = "Rogers—Ramanujan type identities"
